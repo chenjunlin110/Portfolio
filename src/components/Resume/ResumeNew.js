575f9596592efa5b53d5ff90a6df4729569ec50e
chenjunlin110 <33899593+chenjunlin110@users.noreply.github.com>
2024-11-27 00:47:23 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Junlin_Chen_Resume.pdf"; // Replace with your actual resume file path
+import pdf from "../../Assets/../Assets/resume.pdf"; // Replace with your actual resume file path
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
